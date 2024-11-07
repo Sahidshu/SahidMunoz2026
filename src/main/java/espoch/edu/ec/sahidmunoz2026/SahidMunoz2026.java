1956ecb7c151f13c880b64e9b98592d13f5023c4
Sahidshu <sahidmunoz0@gmail.com>
2024-11-07 13:20:53 -0500

    Practica 2

---
+++ src/main/java/espoch/edu/ec/sahidmunoz2026/SahidMunoz2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.sahidmunoz2026;
+
+/**
+ *
+ * @author sahid
+ */
+public class SahidMunoz2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
